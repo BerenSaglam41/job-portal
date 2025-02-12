1be5253ea6db6cc2fff3d261510ad0916a779b4e
BerenSaglam41 <mustafaberensaglam9@gmail.com>
2025-02-12 16:34:45 +0300

    sa

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,15 +1,17 @@
 import { Webhook } from "svix";
 import User from '../models/User.js';
+import dotenv from 'dotenv'
+dotenv.config()
 
 // API Controller function to manage clerk user with database
 export const clerkWebHooks = async (req,res) =>{
     try {
+        console.log("selam");
         // Create a Svix instance with clerk webhook secret.
-        console.log(CLERK_WEBHOOK_SECRET);
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
         
         // Verify headers
-        await whook.verify(JSON.stringify(req,body),{
+        await whook.verify(JSON.stringify(req.body),{
             "svix-id" : req.headers["svix-id"],
             "svix-timestamp" : req.headers["svix-timestamp"],
             "svix-signature" : req.headers["svix-signature"]
